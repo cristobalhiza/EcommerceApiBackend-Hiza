40e06a00fc9e46699dbe2d37eaaa2ef219529c9d
cristobalhiza <cristobalhiza@gmail.com>
2025-02-06 20:33:57 -0300

    remove bcrypt add bcryptjs

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,20 @@
-# Etapa base
-FROM node:20-slim AS base
+FROM node:20-slim
+
 ENV PNPM_HOME="/pnpm"
 ENV PATH="$PNPM_HOME:$PATH"
-RUN corepack enable
+
+RUN npm install -g pnpm
+
 WORKDIR /app
+
+COPY package.json pnpm-lock.yaml ./
+
+RUN pnpm install --prod --frozen-lockfile --shamefully-hoist
+
 COPY . .
 
-FROM base AS prod-deps
-RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --prod --frozen-lockfile --shamefully-hoist
-
-FROM base AS build
-RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile --shamefully-hoist
-RUN pnpm run build
-
-FROM node:20-slim
-WORKDIR /app
-COPY --from=prod-deps /app/node_modules /app/node_modules
-COPY --from=build /app/dist /app/dist
 ENV NODE_ENV=production
+
 EXPOSE 8080
-CMD ["node", "./dist/app.js"]
+
+CMD ["node", "./src/app.js"]
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs';
 import passport from 'passport';
 import mongoose from 'mongoose';
 import loggerUtil from './logger.util.js';
@@ -8,8 +8,8 @@ import loggerUtil from './logger.util.js';
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
 
-export const generaHash = password => {
-    const salt = bcrypt.genSaltSync(10);
+export const generaHash = (password) => {
+    const salt = bcrypt.genSaltSync(10); 
     return bcrypt.hashSync(password, salt);
 };
 
@@ -19,18 +19,16 @@ export const comparaPassword = (password, hash) => {
 
 export const errorHandler = (res, error) => {
     loggerUtil.FATAL('Error en el servidor:', error);
-    const statusCode = error.statusCode || 500
-    const message = error.message || "FATAL ERROR"
-    if (statusCode.startsWith("4")) {
-        loggerUtil.WARN(error)
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'FATAL ERROR';
+    if (statusCode.toString().startsWith('4')) {
+        loggerUtil.WARN(error);
     } else {
-        loggerUtil.FATAL(error)
+        loggerUtil.FATAL(error);
     }
-    return res
-    .status(statusCode)
-    .json({
+    return res.status(statusCode).json({
         error: 'Error inesperado en el servidor. Por favor, intente más tarde.',
-        ...(process.env.NODE_ENV !== 'production' && { detalle: message })
+        ...(process.env.NODE_ENV !== 'production' && { detalle: message }),
     });
 };
 
@@ -38,7 +36,8 @@ export const passportCall = (estrategia) => (req, res, next) => {
     passport.authenticate(estrategia, { session: false }, function (err, user, info) {
         if (err) {
             console.error('Error en Passport:', err);
-            return res.status(500).json({ error: 'Error inesperado en autenticación.' });        }
+            return res.status(500).json({ error: 'Error inesperado en autenticación.' });
+        }
         if (!user) {
             return res.status(401).json({
                 error: info?.message || 'Usuario no autorizado. Verifica tus credenciales.',
